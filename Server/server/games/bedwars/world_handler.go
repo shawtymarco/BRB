701ccb3c36ff7d24e0e3880957dd2ac8e64f2d53
Studgi <stodgytomcat565@gmail.com>
2025-07-25 09:27:06 +0300

    - fix silly bug - remove sound

--- Server/server/games/bedwars/world_handler.go
+++ Server/server/games/bedwars/world_handler.go
@@ -3,8 +3,6 @@ package bedwars
 import (
 	"server/server/blocks/bed"
 
-	"github.com/df-mc/dragonfly/server/world/sound"
-
 	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/df-mc/dragonfly/server/world"
 	"github.com/go-gl/mathgl/mgl64"
@@ -14,12 +12,6 @@ type WorldHandler struct {
 	world.NopHandler
 
 	game *BedWars
-}
-
-func (h WorldHandler) HandleSound(ctx *world.Context, s world.Sound, pos mgl64.Vec3) {
-	if _, ok := s.(sound.Attack); ok {
-		ctx.Cancel()
-	}
 }
 
 func (h WorldHandler) HandleExplosion(ctx *world.Context, position mgl64.Vec3, entities *[]world.Entity, blocks *[]cube.Pos, itemDropChance *float64, spawnFire *bool) {
